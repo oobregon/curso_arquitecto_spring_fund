f7b38ea4b3343b2310984af37050d53d446d9e60
oobregon <oobregonmartin@gmail.com>
2019-11-05 10:51:06 +0100

    cambios

--- 09_escuela_jsf_spring_jpa_hib/src/managed/CursosBean.java
+++ 09_escuela_jsf_spring_jpa_hib/src/managed/CursosBean.java
@@ -8,7 +8,6 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import dto.CursoView;
-import model.Curso;
 import servicio.ServicioEscuela;
 
 @ManagedBean(name = "cursosBean")
